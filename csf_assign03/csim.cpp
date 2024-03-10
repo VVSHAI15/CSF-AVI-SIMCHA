2441de0a0ba8728bafce7c1624297cbec5d1b1ea
AviShai Dayanim <adayani1@jh.edu>
2024-03-09 23:55:23 -0500

    Validity checks for args

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -1,8 +1,26 @@
+#include "cache.h"
+#include "helper_functions.h"
 #include <iostream>
 
-int main() {
-    // Fantastic mindblowing code
-    
-    std::cout << "Goodbye World" <<std::endl;
-    return 0; // Indicate successful execution
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+    //Handel Cache configuration
+    bool wrAlloc = false;
+    bool wrThrough = false;
+    bool Fifo = false;
+    int isInvalid = checkArgValidity(argc, argv, &wrAlloc, &wrThrough, &Fifo);
+    if (isInvalid != 0) {
+        return isInvalid;
+    }
+
+    //store args as ints
+    uint32_t numSets = stoul(argv[1]);
+    uint32_t numBlocks = stoul(argv[2]);
+    uint32_t blockSize = stoul(argv[3]);
+    int logSets = logBase2(nSets);
+    int logBlockSize = logBase2(blockSize); 
+
+    return 0; 
 }
