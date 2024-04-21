72a01eddea3393235a63446311204b516bcae44a
Stuart Malina <smalina2@jhu.edu>
2024-04-21 16:14:57 -0400

    increment value

--- csf_assign05/incr_value.cpp
+++ csf_assign05/incr_value.cpp
@@ -18,16 +18,22 @@ std::string extractValueBetweenQuotes(const std::string &input) {
     return input.substr(start + 1, end - start - 1);
 }
 
+void send_message(int fd, const std::string &msg) {
+    if (rio_writen(fd, msg.c_str(), msg.size()) != static_cast<ssize_t>(msg.size())) {
+        throw CommException("Failed to send message");
+    }
+}
+
 std::string read_response(int fd, rio_t &rio) {
-  char buf[MAXLINE];
-  if (rio_readlineb(&rio, buf, MAXLINE) <= 0) {
-    throw CommException("Failed to read response from server");
-  }
-  std::string response(buf);
-  if (response.empty() || response.back() != '\n') {
-    throw InvalidMessage("Server response not properly terminated");
-  }
-  return response.substr(0, response.size() - 1);
+    char buf[MAXLINE];
+    if (rio_readlineb(&rio, buf, MAXLINE) < 0) {
+        throw CommException("Failed to read response from server");
+    }
+    std::string response(buf);
+    if (response.empty() || response.back() != '\n') {
+        throw InvalidMessage("Server response not properly terminated");
+    }
+    return response.substr(0, response.size() - 1);
 }
 
 int main(int argc, char **argv) {
@@ -56,7 +62,7 @@ int main(int argc, char **argv) {
     send_message(clientfd, "LOGIN " + username + "\n");
     std::string rep_login = read_response(clientfd, rio);
     if (rep_login != "OK") {
-      std::string error_message = extractQuotedMessage(rep_login);
+      std::string error_message = extractValueBetweenQuotes(rep_login);
       if (error_message.empty()) {
         throw InvalidMessage("Failed to login");
       } else {
@@ -68,7 +74,7 @@ int main(int argc, char **argv) {
       send_message(clientfd, "BEGIN\n");
       std::string rep_begin = read_response(clientfd, rio);
       if (rep_begin != "OK") {
-        std::string error_message = extractQuotedMessage(rep_begin);
+        std::string error_message = extractValueBetweenQuotes(rep_begin);
         if (error_message.empty()) {
           throw OperationException("Failed to begin transaction");
         } else {
@@ -80,7 +86,7 @@ int main(int argc, char **argv) {
     send_message(clientfd, "GET " + table + " " + key + "\n");
     std::string rep_get = read_response(clientfd, rio);
     if (rep_get != "OK") {
-      std::string error_message = extractQuotedMessage(rep_get);
+      std::string error_message = extractValueBetweenQuotes(rep_get);
       if (error_message.empty()) {
         throw InvalidMessage("Failed to get value");
       } else {
@@ -91,7 +97,7 @@ int main(int argc, char **argv) {
     send_message(clientfd, "PUSH 1\n");
     std::string rep_push = read_response(clientfd, rio);
     if (rep_push != "OK") {
-      std::string error_message = extractQuotedMessage(rep_push);
+      std::string error_message = extractValueBetweenQuotes(rep_push);
       if (error_message.empty()) {
         throw OperationException("Failed to push value");
       } else {
@@ -102,7 +108,7 @@ int main(int argc, char **argv) {
     send_message(clientfd, "ADD\n");
     std::string rep_add = read_response(clientfd, rio);
     if (rep_add != "OK") {
-      std::string error_message = extractQuotedMessage(rep_add);
+      std::string error_message = extractValueBetweenQuotes(rep_add);
       if (error_message.empty()) {
         throw OperationException("Failed to add value");
       } else {
@@ -113,7 +119,7 @@ int main(int argc, char **argv) {
     send_message(clientfd, "SET " + table + " " + key + "\n");
     std::string rep_set = read_response(clientfd, rio);
     if (rep_set != "OK") {
-      std::string error_message = extractQuotedMessage(rep_set);
+      std::string error_message = extractValueBetweenQuotes(rep_set);
       if (error_message.empty()) {
         throw OperationException("Failed to set value");
       } else {
@@ -125,7 +131,7 @@ int main(int argc, char **argv) {
       send_message(clientfd, "COMMIT\n");
       std::string rep_commit = read_response(clientfd, rio);
       if (rep_commit != "OK") {
-        std::string error_message = extractQuotedMessage(rep_commit);
+        std::string error_message = extractValueBetweenQuotes(rep_commit);
         if (error_message.empty()) {
           throw OperationException("Failed to commit transaction");
         } else {
