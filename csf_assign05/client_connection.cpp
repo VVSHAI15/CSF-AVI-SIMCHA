ca0772f26fee4251e182f0432aeaff8fb372816b
Stuart Malina <smalina2@jhu.edu>
2024-04-26 14:43:18 -0400

    some skeleton of client connection

--- csf_assign05/client_connection.cpp
+++ csf_assign05/client_connection.cpp
@@ -4,52 +4,232 @@
 #include "message.h"
 #include "message_serialization.h"
 #include "server.h"
-#include <cassert>
 #include <iostream>
+#include <cassert>
 
 ClientConnection::ClientConnection(Server *server, int client_fd)
-    : m_server(server), m_client_fd(client_fd), in_transactions(false),
+    : m_server(server), m_client_fd(client_fd), in_transaction(false), 
       stack(new ValueStack()) {
   rio_readinitb(&m_fdbuf, m_client_fd);
 }
 
-ClientConnection::~ClientConnection() { Close(m_client_fd); }
-// TODO Maintain a way to insure that the client LOGIN before any other
-// operation
+ClientConnection::~ClientConnection() {
+  Close(m_client_fd);
+  delete stack;
+}
+
+
+
 void ClientConnection::chat_with_client() {
   bool ongoing = true;
-  int message_counter = 0;
 
   while (ongoing) {
-    std::string encoded_message;
     Message message;
-    try {
-
-    } catch
-  }
-
-    Message message;
-    switch (message.get_message_type()) {
-    case MessageType::NONE:
-    case MessageType::LOGIN:
-    case MessageType::CREATE:
-    case MessageType::PUSH:
-    case MessageType::POP:
-    case MessageType::TOP:
-    case MessageType::SET:
-    case MessageType::GET:
-    case MessageType::ADD:
-    case MessageType::SUB:
-    case MessageType::MUL:
-    case MessageType::DIV:
-    case MessageType::BEGIN:
-    case MessageType::COMMIT:
-    case MessageType::BYE:
-    case MessageType::OK:
-    case MessageType::FAILED:
-    case MessageType::ERROR:
-    case MessageType::DATA:
-    default:
-      break; // TODO
+    char buf[MAXLINE]; 
+    if (Rio_readlineb(&m_fdbuf, buf, MAXLINE) < 0) {
+      throw CommException("Failed to read from client");
     }
+    MessageSerialization::decode(buf, message);
+
+    switch (message.get_message_type()) {
+      case MessageType::LOGIN:
+        handle_login(message);
+        break;
+      case MessageType::CREATE:
+        handle_create(message);
+        break;
+      case MessageType::SET:
+        handle_set(message);
+        break;
+      case MessageType::GET:
+        handle_get(message);
+        break;
+      case MessageType::PUSH:
+        handle_push(message);
+        break;
+      case MessageType::POP:
+        handle_pop();
+        break;
+      case MessageType::TOP:
+        handle_top();
+        break;
+      case MessageType::ADD:
+        handle_add();
+        break;
+      case MessageType::SUB:
+        handle_sub();
+        break;
+      case MessageType::MUL:
+        handle_mul();
+        break;
+      case MessageType::DIV:
+        handle_div();
+        break;
+      case MessageType::BEGIN:
+        handle_begin();
+        break;
+      case MessageType::COMMIT:
+        handle_commit();
+        break;
+      case MessageType::BYE:
+        ongoing = false;
+        send_response(MessageType::OK);
+        break;
+      default:
+        send_response(MessageType::ERROR, "Unsupported operation");
+        break;
+    }
+  }
+} 
+
+
+void ClientConnection::handle_push(const Message& message) {
+  stack->push(message.get_value());
+  send_response(MessageType::OK);
 }
+
+void ClientConnection::handle_pop() {
+  if (!stack->is_empty()) {
+    stack->pop();
+    send_response(MessageType::OK);
+  } else {
+    send_response(MessageType::ERROR, "Stack empty");
+  }
+}
+
+void ClientConnection::handle_top() {
+  if (!stack->is_empty()) {
+    send_response(MessageType::DATA, stack->get_top());
+  } else {
+    send_response(MessageType::ERROR, "Stack empty");
+  }
+}
+
+void ClientConnection::handle_add() {
+    // must check the stack has 2 left 
+    int num1 = std::stoi(stack->get_top()); stack->pop();
+    int num2 = std::stoi(stack->get_top()); stack->pop();
+    int sum = num1 + num2;
+    stack->push(std::to_string(sum));
+    send_response(MessageType::OK);
+}
+
+void ClientConnection::handle_sub() {
+  // must check the stack has 2 left 
+  int right = std::stoi(stack->get_top()); stack->pop();
+  int left = std::stoi(stack->get_top()); stack->pop();
+  int difference = left - right;
+  stack->push(std::to_string(difference));
+  send_response(MessageType::OK);
+}
+
+void ClientConnection::handle_mul() {
+    // must check the stack has 2 left 
+  int first = std::stoi(stack->get_top()); stack->pop();
+  int second = std::stoi(stack->get_top()); stack->pop();
+  int product = first * second;
+  stack->push(std::to_string(product));
+  send_response(MessageType::OK);
+}
+
+void ClientConnection::handle_div() {
+  // we must check the stack has 2 left 
+  int divisor = std::stoi(stack->get_top()); stack->pop();
+  if (divisor == 0) {
+    send_response(MessageType::ERROR, "Division by zero");
+    return;
+  }
+  int dividend = std::stoi(stack->get_top()); stack->pop();
+  int quotient = dividend / divisor;
+  stack->push(std::to_string(quotient));
+  send_response(MessageType::OK);
+}
+
+void ClientConnection::handle_login(const Message& message) {
+  // TODO 
+  send_response(MessageType::OK);
+}
+
+void ClientConnection::handle_create(const Message& message) {
+  std::string tableName = message.get_table();
+  if (m_server->find_table(tableName)) {
+    send_response(MessageType::FAILED, {"Table already exists"});
+  } else {
+    m_server->create_table(tableName);
+    send_response(MessageType::OK, {});
+  }
+}
+
+void ClientConnection::handle_set(const Message& message) {
+  std::string tableName = message.get_table();
+  std::string key = message.get_key();
+  std::string value = stack->get_top();  // we are assuming value to set is on top of the stack
+  Table* table = m_server->find_table(tableName);
+  if (!table) {
+    send_response(MessageType::ERROR, {"Table not found"});
+    return;
+  }
+  if (in_transaction && !locked_tables.count(tableName)) {
+    if (!table->trylock()) {
+      send_response(MessageType::FAILED, {"Lock failed"});
+      return;
+    }
+    locked_tables.insert(tableName);
+  } else {
+    table->lock();
+  }
+  table->set(key, value, in_transaction);
+  send_response(MessageType::OK, {});
+  if (!in_transaction) {
+    table->unlock();
+  }
+}
+
+void ClientConnection::handle_get(const Message& message) {
+  std::string tableName = message.get_table();
+  std::string key = message.get_key();
+  Table* table = m_server->find_table(tableName);
+  if (!table) {
+    send_response(MessageType::ERROR, {"Table not found"});
+    return;
+  }
+  table->lock();
+  try {
+    std::string value = table->get(key, in_transaction);
+    stack->push(value);
+    send_response(MessageType::OK, {});
+  } catch (const std::exception& e) {
+    send_response(MessageType::ERROR, {e.what()});
+  }
+  table->unlock();
+}
+
+
+void ClientConnection::handle_begin() {
+  if (in_transaction) {
+    send_response(MessageType::FAILED, {"Transaction already started"});
+  } else {
+    in_transaction = true;
+    send_response(MessageType::OK, {});
+  }
+}
+
+void ClientConnection::handle_commit() {
+  // ToDO Commit transaction
+  in_transaction = false;
+  send_response(MessageType::OK);
+}
+
+void ClientConnection::send_response(MessageType type, const std::string& additional_info = "") {
+  Message response(type, {additional_info});
+  std::string encoded;
+  MessageSerialization::encode(response, encoded);
+  rio_writen(m_client_fd, encoded.data(), encoded.size());
+}
+
+
+
+
+
+
+
--- csf_assign05/client_connection.h
+++ csf_assign05/client_connection.h
@@ -3,38 +3,45 @@
 
 #include "csapp.h"
 #include "message.h"
+#include "server.h"
 #include "value_stack.h"
 #include <set>
+#include <string>
 
-class Server; // forward declaration
-class Table;  // forward declaration
+class Server;  // Forward declaration to resolve circular dependency
 
 class ClientConnection {
-private:
-  Server *m_server;
-  int m_client_fd;
-  rio_t m_fdbuf;
-  std::vector<std::string> locked_tables;
-  bool in_transactions;
-
-  ValueStack *stack;
-
-  // copy constructor and assignment operator are prohibited
-  ClientConnection(const ClientConnection &);
-  ClientConnection &operator=(const ClientConnection &);
-
 public:
-  ClientConnection(Server *server, int client_fd);
-  ~ClientConnection();
+    ClientConnection(Server *server, int client_fd);
+    ~ClientConnection();
 
-  void chat_with_client();
-  void process_message(const Message &message);
-  void handle_exceptions(const std::exception &e);
-  void commit_transactions();
-  void rollback_transactions();
-  int get_client_fd() const { return m_client_fd; }
+    void chat_with_client(); 
 
-  // TODO: additional member functions
+private:
+    Server *m_server;         // Pointer to server object managing this connection
+    int m_client_fd;          // File descriptor for the client socket
+    rio_t m_fdbuf;            // Buffered file descriptor info for robust I/O
+    std::set<std::string> locked_tables;  // Track tables locked in transaction
+    bool in_transaction;      // Flag to check if currently in a transaction
+    ValueStack *stack;        // Pointer to the stack used for operations
+
+    // Helper methods for handling different message types
+    void handle_login(const Message& message);
+    void handle_create(const Message& message);
+    void handle_set(const Message& message);
+    void handle_get(const Message& message);
+    void handle_push(const Message& message);
+    void handle_pop();
+    void handle_top();
+    void handle_add();
+    void handle_sub();
+    void handle_mul();
+    void handle_div();
+    void handle_begin();
+    void handle_commit();
+    void send_response(MessageType type, const std::string& additional_info = "");
+
+
 };
 
 #endif // CLIENT_CONNECTION_H
